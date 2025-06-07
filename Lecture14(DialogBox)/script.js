77ae425e2c4213ef1f355e30d360d3adae949113
jhapriya909 <thepriyajha8840@gmail.com>
2025-06-07 19:06:45 +0530

    d:\JS Class\Daily-Class_JS\Lecture14(DialogBox)\script.js

---
+++ Lecture14(DialogBox)/script.js
@@ -0,0 +1,9 @@
+// const  alertResult = alert("Your message goes here ");
+
+// const isConfirmed = confirm("Be sure to do your payment after final payment no money will be refunded would you still like to preoceed");
+
+
+const userInput = prompt("Please enter your name : ");
+
+
+// full stack = frontend + backend + cloud data storage + javascript 

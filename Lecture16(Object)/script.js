a58b3f05569f09a1564f7d20fb904b0abb42d3b7
jhapriya909 <thepriyajha8840@gmail.com>
2025-06-07 22:40:42 +0530

    d:\JS Class\Daily-Class_JS\Lecture16(Object)\script.js

---
+++ Lecture16(Object)/script.js
@@ -0,0 +1,17 @@
+// const user = {
+//     firstName: "Shivam",
+//     lastName: "Kumar",
+//     age: 30,  // Age as a number, not a string
+//     gender: "Male",
+//     email: "shivam.kumar@example.com",
+//     phone: "+91-9876543210",
+// };
+
+
+const myName = 'Shivam'
+
+const username1 = ''
+const username2 = ''
+
+const user1 = {}
+const user2 = {}
